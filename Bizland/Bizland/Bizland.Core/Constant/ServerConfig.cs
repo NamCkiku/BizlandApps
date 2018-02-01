b037e4aee7b59f0420aac7ea6d563b55d6a74eaf
NamCkiku <NamCkiku@PPM07-PC>
2018-02-01 10:56:07 +0700

    add packages

---
+++ Bizland/Bizland/Bizland.Core/Constant/ServerConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bizland.Core.Constant
+{
+    /// <summary>
+    /// Thông tin cấu hình của server
+    /// Tất cả thông tin cấu hình cho vào đây để không bị loãng
+    /// </summary>
+    /// <Modified>
+    /// Name     Date         Comments
+    /// TrungTQ  16/1/2018   created
+    /// </Modified>
+    public class ServerConfig
+    {
+        public const string ApiEndpoint = "http://api.bizland.vn";
+        /// <summary>
+        /// Key cho Google Map cho iOS
+        /// </summary>
+        /// <Modified>
+        /// Name     Date         Comments
+        /// TrungTQ  16/1/2018   created
+        /// </Modified>
+        public static string GoogleMapKeyiOS = "";
+
+        /// <summary>
+        /// Key cho Google Map cho Android
+        /// </summary>
+        /// <Modified>
+        /// Name     Date         Comments
+        /// TrungTQ  16/1/2018   created
+        /// </Modified>
+        public static string GoogleMapKeyAndroid = "";
+    }
+}
