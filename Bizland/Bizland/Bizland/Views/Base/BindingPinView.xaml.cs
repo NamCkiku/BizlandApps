c503f6e21f0349c66d05ca8a8f1fa2a2fe6b1ae8
Trần Hoàng Nam <namth@hn-binhanh.local>
2018-02-22 15:15:19 +0700

    add image pin

---
+++ Bizland/Bizland/Bizland/Views/Base/BindingPinView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bizland.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BindingPinView : StackLayout
+    {
+        private string _imagePin;
+        public BindingPinView(string image)
+        {
+            InitializeComponent();
+            _imagePin = image;
+            BindingContext = this;
+        }
+        public string ImagePin
+        {
+            get { return _imagePin; }
+        }
+    }
+}
