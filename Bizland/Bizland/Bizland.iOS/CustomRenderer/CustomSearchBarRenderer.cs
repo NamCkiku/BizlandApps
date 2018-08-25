37dea994329c1f9209a760a58e0cc9c111997152
NamCkiku <NamCkiku@DESKTOP-M6MBQ1L>
2018-08-25 09:10:43 +0700

    custom search bar ios

---
+++ Bizland/Bizland/Bizland.iOS/CustomRenderer/CustomSearchBarRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Bizland.CustomControl;
+using Bizland.iOS.CustomRenderer;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRendererAttribute(typeof(CustomSearchBar), typeof(CustomSearchBarRenderer))]
+namespace Bizland.iOS.CustomRenderer
+{
+    public class CustomSearchBarRenderer : SearchBarRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> args)
+        {
+            base.OnElementChanged(args);
+
+            if (args.OldElement != null || args.NewElement == null)
+                return;
+
+            var csb = (CustomSearchBar)Element;
+            UISearchBar bar = (UISearchBar)this.Control;
+
+            bar.AutocapitalizationType = UITextAutocapitalizationType.AllCharacters;
+            bar.AutocorrectionType = UITextAutocorrectionType.Yes;
+            bar.BarStyle = (UIBarStyle)Enum.Parse(typeof(UIBarStyle), csb.BarStyle);
+            bar.BarTintColor = csb.BarTint.GetValueOrDefault().ToUIColor();
+            bar.KeyboardType = UIKeyboardType.ASCIICapable;
+            bar.SearchBarStyle = (UISearchBarStyle)Enum.Parse(typeof(UISearchBarStyle), csb.BarStyle);
+            bar.ShowsScopeBar = true;
+            bar.ShowsCancelButton = csb.CancelButtonIsVisible;
+        }
+    }
+}
---
+++ Bizland/Bizland/Bizland/CustomControl/CustomSearchBar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Bizland.CustomControl
+{
+    public class CustomSearchBar : SearchBar
+    {
+        // Use Bindable properties to maintain XAML binding compatibility
+
+        public static BindableProperty BarTintProperty = BindableProperty.Create(nameof(CustomSearchBar), typeof(Color), typeof(CustomSearchBar), Color.Accent);
+        public Color? BarTint
+        {
+            get { return (Color?)GetValue(BarTintProperty); }
+            set { SetValue(BarTintProperty, value); }
+        }
+        public static BindableProperty SearchStyleProperty = BindableProperty.Create(nameof(CustomSearchBar), typeof(string), typeof(CustomSearchBar), "Default");
+        public string SearchStyle
+        {
+            get { return (string)GetValue(SearchStyleProperty); }
+            set { SetValue(SearchStyleProperty, value); }
+        }
+        public static BindableProperty BarStyleProperty = BindableProperty.Create(nameof(CustomSearchBar), typeof(string), typeof(CustomSearchBar), "Default");
+        public string BarStyle
+        {
+            get { return (string)GetValue(BarStyleProperty); }
+            set { SetValue(BarStyleProperty, value); }
+        }
+        public static BindableProperty CancelButtonIsVisibleProperty = BindableProperty.Create(nameof(CustomSearchBar), typeof(bool), typeof(CustomSearchBar), true);
+        public bool CancelButtonIsVisible
+        {
+            get { return (bool)GetValue(CancelButtonIsVisibleProperty); }
+            set { SetValue(CancelButtonIsVisibleProperty, value); }
+        }
+    }
+}
