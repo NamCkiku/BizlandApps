79178ba27258114b808145c83875ea085286fec7
NamCkiku <tranhoangnam11373@gmail.com>
2018-10-08 10:52:20 +0700

    add hamburger

--- Bizland/Bizland/Bizland.iOS/CustomRenderer/ContentPageRenderer.cs
+++ Bizland/Bizland/Bizland.iOS/CustomRenderer/ContentPageRenderer.cs
@@ -30,7 +30,7 @@ namespace Bizland.iOS.CustomRenderer
             UINavigationBar.Appearance.SetBackgroundImage(new UIImage(), UIBarMetrics.Default);
             UINavigationBar.Appearance.ShadowImage = new UIImage();
             UINavigationBar.Appearance.BackgroundColor = UIColor.Clear;
-            UINavigationBar.Appearance.TintColor = UIColor.Clear;
+            UINavigationBar.Appearance.TintColor = UIColor.White;
             UINavigationBar.Appearance.BarTintColor = UIColor.Clear;
             UINavigationBar.Appearance.Translucent = false;
             UINavigationBar.Appearance.BarStyle = UIBarStyle.Black;
