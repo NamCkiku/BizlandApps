427b891debbe98c86151424133573985461f27a1
NamCkiku <tranhoangnam11373@gmail.com>
2018-10-08 09:49:28 +0700

    fix transparent bar ios

--- Bizland/Bizland/Bizland.iOS/CustomRenderer/TransparentNavigationRenderer.cs
+++ Bizland/Bizland/Bizland.iOS/CustomRenderer/TransparentNavigationRenderer.cs
@@ -17,7 +17,7 @@ namespace Bizland.iOS.CustomRenderer
             UINavigationBar.Appearance.ShadowImage = new UIImage();
             UINavigationBar.Appearance.BackgroundColor = UIColor.Clear;
             UINavigationBar.Appearance.TintColor = UIColor.White;
-            UINavigationBar.Appearance.BarTintColor = UIColor.Clear;
+            UINavigationBar.Appearance.BarTintColor = UIColor.Black;
             UINavigationBar.Appearance.Translucent = true;
         }
 
