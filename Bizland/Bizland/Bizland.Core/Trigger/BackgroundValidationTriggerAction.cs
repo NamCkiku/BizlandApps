d1e714768d91c65fe098a8716105e9f2d01bf8d2
NamCkiku <tranhoangnam11373@gmail.com>
2018-10-19 14:31:00 +0700

    add validation

---
+++ Bizland/Bizland/Bizland.Core/Trigger/BackgroundValidationTriggerAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Bizland.Core.Trigger
+{
+    public class BackgroundValidationTriggerAction : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry entry)
+        {
+            double result;
+            bool isValid = Double.TryParse(entry.Text, out result);
+            entry.PlaceholderColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+}
