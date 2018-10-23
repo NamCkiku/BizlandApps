1f4eb8eee10f65aff13622e497373152b11586ed
NamCkiku <tranhoangnam11373@gmail.com>
2018-10-23 15:52:08 +0700

    add validation rule

---
+++ Bizland/Bizland/Bizland.Core/Validations/ObjectNullRule.cs
@@ -0,0 +1,16 @@
+﻿namespace Bizland.Core
+{
+    public class ObjectNullRule<T> : IValidationRule<T>
+    {
+        public string ValidationMessage { get; set; }
+
+        public bool Check(T value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+            else { return true}
+        }
+    }
+}
--- Bizland/Bizland/Bizland/ViewModels/Room/CreateRoomPageViewModel.cs
+++ Bizland/Bizland/Bizland/ViewModels/Room/CreateRoomPageViewModel.cs
@@ -6,6 +6,7 @@ using Bizland.Model;
 using Bizland.Views;
 using Prism.Events;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.ObjectModel;
 using System.Reflection;
@@ -18,12 +19,14 @@ namespace Bizland.ViewModels
         private readonly INavigationService _navigationService;
         private readonly IEventAggregator _eventAggregator;
         private readonly IRoomTypeService _roomTypeService;
-        public CreateRoomPageViewModel(INavigationService navigationService, IRoomTypeService roomTypeService, IEventAggregator eventAggregator)
+        private readonly IPageDialogService _dialogService;
+        public CreateRoomPageViewModel(INavigationService navigationService, IRoomTypeService roomTypeService, IEventAggregator eventAggregator, IPageDialogService dialogService)
              : base(navigationService)
         {
             this._navigationService = navigationService;
             this._eventAggregator = eventAggregator;
             this._roomTypeService = roomTypeService;
+            this._dialogService = dialogService;
             Title = "Đăng phòng";
 
             eventAggregator.GetEvent<SelectProvinceEvent>().Subscribe(UpdateProvince);
@@ -32,6 +35,11 @@ namespace Bizland.ViewModels
             _roomName = new ValidatableObject<string>();
             _priceRoom = new ValidatableObject<int>();
             _roomType = new ValidatableObject<RoomType>();
+            _province = new ValidatableObject<Province>();
+            _acreage = new ValidatableObject<int>();
+            _phonenumber = new ValidatableObject<int>();
+            _address = new ValidatableObject<string>();
+            _note = new ValidatableObject<string>();
             AddValidations();
         }
         #region private Method
@@ -51,7 +59,7 @@ namespace Bizland.ViewModels
 
         private void UpdateProvince(Province param)
         {
-            Province = param;
+            Province.Value = param;
         }
         private void UpdateDistrict(District param)
         {
@@ -61,8 +69,13 @@ namespace Bizland.ViewModels
         private void AddValidations()
         {
             _roomName.Validations.Add(new IsNotNullOrEmptyRule<string> { ValidationMessage = "Điền đầy đủ các thông tin để đăng phòng" });
-            _priceRoom.Validations.Add(new IsNotNullOrEmptyRule<int> { ValidationMessage = "Điền đầy đủ các thông tin để đăng phòng" });
-            _roomType.Validations.Add(new IsNotNullOrEmptyRule<RoomType> { ValidationMessage = "Điền đầy đủ các thông tin để đăng phòng" });
+            _priceRoom.Validations.Add(new IntRule<int> { ValidationMessage = "Điền đầy đủ các thông tin để đăng phòng" });
+            _roomType.Validations.Add(new ObjectNullRule<RoomType> { ValidationMessage = "Điền đầy đủ các thông tin để đăng phòng" });
+            _province.Validations.Add(new ObjectNullRule<Province> { ValidationMessage = "Điền đầy đủ các thông tin để đăng phòng" });
+            _acreage.Validations.Add(new IntRule<int> { ValidationMessage = "Điền đầy đủ các thông tin để đăng phòng" });
+            _phonenumber.Validations.Add(new IntRule<int> { ValidationMessage = "Điền đầy đủ các thông tin để đăng phòng" });
+            _address.Validations.Add(new IsNotNullOrEmptyRule<string> { ValidationMessage = "Điền đầy đủ các thông tin để đăng phòng" });
+            _note.Validations.Add(new IsNotNullOrEmptyRule<string> { ValidationMessage = "Điền đầy đủ các thông tin để đăng phòng" });
         }
 
         private bool Validate()
@@ -71,15 +84,21 @@ namespace Bizland.ViewModels
             bool isValidRoomName = _roomName.Validate();
             bool isValidPrice = _priceRoom.Validate();
             bool isValidRoomType = _roomType.Validate();
+            bool isValidprovince = _province.Validate();
+            bool isValidacreage = _acreage.Validate();
+            bool isValidphonenumber = _phonenumber.Validate();
+            bool isValidaddress = _address.Validate();
+            bool isValidnote = _note.Validate();
 
-            return isValidRoomName && isValidPrice && isValidRoomType;
+            return isValidRoomName && isValidPrice && isValidRoomType && isValidprovince
+                && isValidacreage && isValidphonenumber && isValidaddress && isValidnote;
         }
         #endregion
 
 
         #region Property
-        private Province _province;
-        public Province Province
+        private ValidatableObject<Province> _province;
+        public ValidatableObject<Province> Province
         {
             get { return _province; }
             set
@@ -157,6 +176,62 @@ namespace Bizland.ViewModels
                 RaisePropertyChanged(() => PriceRoom);
             }
         }
+        private ValidatableObject<int> _acreage;
+        public ValidatableObject<int> Acreage
+        {
+            get
+            {
+                return _acreage;
+            }
+            set
+            {
+                _acreage = value;
+                RaisePropertyChanged(() => Acreage);
+            }
+        }
+
+        private ValidatableObject<int> _phonenumber;
+        public ValidatableObject<int> PhoneNumber
+        {
+            get
+            {
+                return _phonenumber;
+            }
+            set
+            {
+                _phonenumber = value;
+                RaisePropertyChanged(() => PhoneNumber);
+            }
+        }
+
+        private ValidatableObject<string> _address;
+        public ValidatableObject<string> Address
+        {
+            get
+            {
+                return _address;
+            }
+            set
+            {
+                _address = value;
+                RaisePropertyChanged(() => Address);
+            }
+        }
+
+        private ValidatableObject<string> _note;
+        public ValidatableObject<string> Note
+        {
+            get
+            {
+                return _note;
+            }
+            set
+            {
+                _note = value;
+                RaisePropertyChanged(() => Note);
+            }
+        }
+
         #endregion
 
         #region Command
@@ -232,9 +307,16 @@ namespace Bizland.ViewModels
                     IsBusy = true;
                     try
                     {
-                        NavigationParameters navigationParameters = new NavigationParameters();
-                        navigationParameters.Add("provinceID", _province.Id);
-                        await _navigationService.NavigateAsync("BaseNavigationPage/DistrictPage", navigationParameters, useModalNavigation: true);
+                        if (Province != null && Province.Value.Id > 0)
+                        {
+                            NavigationParameters navigationParameters = new NavigationParameters();
+                            navigationParameters.Add("provinceID", Province.Value.Id);
+                            await _navigationService.NavigateAsync("BaseNavigationPage/DistrictPage", navigationParameters, useModalNavigation: true);
+                        }
+                        else
+                        {
+                            await _dialogService.DisplayAlertAsync("Thông báo !", "Hãy họn tỉnh thành trước khi chọn quận huyện", "Đồng ý");
+                        }
                     }
                     catch (Exception ex)
                     {
