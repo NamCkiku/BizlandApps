994d2a8ea5776d9a1414e87059553bc82e46ad6c
NamCkiku <tranhoangnam11373@gmail.com>
2018-10-24 16:04:25 +0700

    eqb,podamdfalsm

--- Bizland/Bizland/Bizland.iOS/DependencyService/MediaService.cs
+++ Bizland/Bizland/Bizland.iOS/DependencyService/MediaService.cs
@@ -15,92 +15,6 @@ namespace Bizland.iOS.DependencyService
 {
     public class MediaService : UIViewController, IMediaService
     {
-        public void ClearFiles(List<string> filePaths)
-        {
-            throw new System.NotImplementedException();
-        }
-        private readonly PHAsset[] _preselectedAssets;
-        public void OpenGallery()
-        {
-            var picker = new GMImagePickerController
-            {
-                Title = "Đăng ảnh",
-                CustomDoneButtonTitle = "Hoàn thành",
-                CustomCancelButtonTitle = "Đóng",
-                CustomNavigationBarPrompt = "Chụp ảnh mới hoặc chọn ảnh hiện có",
-                ColsInPortrait = 3,
-                ColsInLandscape = 4,
-                MinimumInteritemSpacing = 2.0f,
-                DisplaySelectionInfoToolbar = true,
-                AllowsMultipleSelection = true,
-                ShowCameraButton = true,
-                AutoSelectCameraImages = true,
-                AllowsEditingCameraImages = true,
-                ModalPresentationStyle = UIModalPresentationStyle.Popover,
-                MediaTypes = new[] { PHAssetMediaType.Image },
-                CustomSmartCollections = new[] { PHAssetCollectionSubtype.AlbumRegular, PHAssetCollectionSubtype.AlbumImported },
-                NavigationBarBackgroundColor = UIColor.White,
-                NavigationBarTextColor = UIColor.Blue,
-                NavigationBarTintColor = UIColor.White,
-                PickerBackgroundColor = UIColor.White,
-                ToolbarBackgroundColor = UIColor.White,
-                ToolbarBarTintColor = UIColor.Blue,
-                ToolbarTextColor = UIColor.Blue,
-                PickerStatusBarStyle = UIStatusBarStyle.Default,
-                GridSortOrder = SortOrder.Descending,
-                AdditionalToolbarItems = new UIBarButtonItem[] { new UIBarButtonItem(UIBarButtonSystemItem.Bookmarks), new UIBarButtonItem("Custom", UIBarButtonItemStyle.Bordered, (s, e) => { Console.WriteLine("test"); }) },
-
-            };
-
-            if (_preselectedAssets != null)
-            {
-                foreach (var asset in _preselectedAssets)
-                {
-                    picker.SelectedAssets.Add(asset);
-                }
-            }
-            // Event handling
-            picker.FinishedPickingAssets += Picker_FinishedPickingAssets;
-            picker.Canceled += Picker_Canceled;
-
-            // GMImagePicker can be treated as a PopOver as well:
-            var popPC = picker.PopoverPresentationController;
-            popPC.PermittedArrowDirections = UIPopoverArrowDirection.Any;
-
-            UIWindow window = UIApplication.SharedApplication.KeyWindow;
-            var viewController = window.RootViewController;
-            viewController.PresentModalViewController(picker, true);
-        }
-
-
-        void Picker_Canceled(object sender, EventArgs e)
-        {
-            if (sender is UIImagePickerController)
-            {
-                ((UIImagePickerController)sender).DismissViewController(true, null);
-            }
-            Console.WriteLine("User canceled picking image.");
-        }
-
-        private void Picker_FinishedPickingAssets(object sender, MultiAssetEventArgs args)
-        {
-            _pickAsyncResult = new List<ImageSource>();
-            PHImageManager imageManager = new PHImageManager();
-            // For demo purposes: just show all chosen pictures in order every second
-            foreach (var asset in args.Assets)
-            {
-                imageManager.RequestImageForAsset(asset,
-                new CGSize(asset.PixelWidth, asset.PixelHeight),
-                PHImageContentMode.Default,
-                null,
-                (image, info) =>
-                {
-                    var imagesource = ImageSource.FromStream(() => image.AsPNG().AsStream());
-                    _pickAsyncResult.Add(imagesource);
-                });
-            }
-            _waitHandle.Set();
-        }
 
         private List<ImageSource> _pickAsyncResult;
         private readonly EventWaitHandle _waitHandle = new AutoResetEvent(false);
@@ -123,24 +37,13 @@ namespace Bizland.iOS.DependencyService
                 AllowsEditingCameraImages = true,
                 ModalPresentationStyle = UIModalPresentationStyle.Popover,
                 MediaTypes = new[] { PHAssetMediaType.Image },
-                CustomSmartCollections = new[] { PHAssetCollectionSubtype.AlbumRegular, PHAssetCollectionSubtype.AlbumImported },
                 NavigationBarBackgroundColor = UIColor.White,
-                NavigationBarTextColor = UIColor.Blue,
-                NavigationBarTintColor = UIColor.White,
-                PickerBackgroundColor = UIColor.White,
-                ToolbarBarTintColor = UIColor.White,
-                ToolbarTextColor = UIColor.Blue,
-                PickerStatusBarStyle = UIStatusBarStyle.Default,
-                GridSortOrder = SortOrder.Descending
+                NavigationBarTextColor = UIColor.Black,
+                NavigationBarTintColor = UIColor.Black,
+                PickerStatusBarStyle = UIStatusBarStyle.BlackOpaque,
+                GridSortOrder = SortOrder.Descending,
             };
 
-            if (_preselectedAssets != null)
-            {
-                foreach (var asset in _preselectedAssets)
-                {
-                    picker.SelectedAssets.Add(asset);
-                }
-            }
             picker.Canceled += Picker_Canceled;
             // Event handling
             picker.FinishedPickingAssets += Picker_FinishedPickingAssets;
@@ -158,12 +61,37 @@ namespace Bizland.iOS.DependencyService
             {
                 _waitHandle.WaitOne();
                 var result = _pickAsyncResult;
-                _pickAsyncResult = null;
 
                 return result;
             });
         }
 
+        void Picker_Canceled(object sender, EventArgs e)
+        {
+            if (sender is UIImagePickerController)
+            {
+                ((UIImagePickerController)sender).DismissViewController(true, null);
+            }
+        }
+
+        private void Picker_FinishedPickingAssets(object sender, MultiAssetEventArgs args)
+        {
+            _pickAsyncResult = new List<ImageSource>();
+            PHImageManager imageManager = new PHImageManager();
+            foreach (var asset in args.Assets)
+            {
+                imageManager.RequestImageForAsset(asset,
+                new CGSize(asset.PixelWidth, asset.PixelHeight),
+                PHImageContentMode.Default,
+                null,
+                (image, info) =>
+                {
+                    var imagesource = ImageSource.FromStream(() => image.AsPNG().AsStream());
+                    _pickAsyncResult.Add(imagesource);
+                });
+            }
+            _waitHandle.Set();
+        }
 
 
         private UIViewController GetTheMostPresentedViewController()
