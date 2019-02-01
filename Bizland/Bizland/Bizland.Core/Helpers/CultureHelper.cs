74a4e9febc9fac74398363fea8d1402d92b32883
NamCkiku <tranhoangnam11373@gmail.com>
2019-02-01 11:18:33 +0700

    add CrossMultilingual

---
+++ Bizland/Bizland/Bizland.Core/Helpers/CultureHelper.cs
@@ -0,0 +1,17 @@
+﻿using Bizland.Core.Resources;
+using Plugin.Multilingual;
+using System.Globalization;
+
+namespace Bizland.Core.Helpers
+{
+    public static class CultureHelper
+    {
+
+        public static void SetCulture()
+        {
+            CrossMultilingual.Current.CurrentCultureInfo = new CultureInfo("vi-VN");
+            AppResource.Culture = CrossMultilingual.Current.CurrentCultureInfo;
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("vi-VN");
+        }
+    }
+}
